d2f4863af47f3e43c51fecce28437e904880a8fe
kubilaybalci123 <kubilaybalci123@gmail.com>
2019-10-08 21:51:33 +0300

    last version of RockPaperScissors

--- myscript.js
+++ myscript.js
@@ -1,84 +1,122 @@
+let computerSelection;
+let playerSelection;
+let round = 0;
+let cs = 0;
+let ps = 0;
 
-      let playerSelection;
-      let computerSelection = ComputerPlay();
-      let cs=0;
-      let ps=0;
-      let round =0;
+var rock = document.getElementById("rock");
+var paper = document.getElementById('paper');
+var scissors = document.getElementById("scissors");
+
+var results = document.getElementById("results");
+var scores = document.getElementById("scores");
+var winner = document.getElementById("winner");
 
 
-    function ComputerPlay(){
-      const rand = Math.random();
-      if (rand<0.34) {
-        return "rock";
-      }else if (rand <= 0.67) {
-        return "paper";
-      }else {
-        return "scissors";}
+
+rock.addEventListener("click", function() {
+  playerSelection = "rock";
+  if (ps == 5 || cs == 5) {
+    return;
+  }
+  playRound()
+});
+
+paper.addEventListener("click", function() {
+  playerSelection = "paper";
+  if (ps == 5 || cs == 5) {
+    return;
+  }
+  playRound()
+});
+
+scissors.addEventListener("click", function() {
+  playerSelection = "scissors";
+  if (ps == 5 || cs == 5) {
+    return;
+  }
+  playRound()
+});
+
+
+
+function playRound() {
+
+  function ComputerPlay() {
+    const rand = Math.random();
+    if (rand < 0.34) {
+      return "rock";
+    } else if (rand <= 0.67) {
+      return "paper";
+    } else {
+      return "scissors";
     }
+  }
 
-    function playRound(playerSelection,computerSelection){
+  computerSelection = ComputerPlay();
 
-      playerSelection = prompt("Which one do you choose Rock, Paper, Scissors ?").toLowerCase();
-
-      if (playerSelection===computerSelection) {
-      return "Tie!"+"Let's play Again.";
-      }
-
-      else if (playerSelection==="rock") {
-        if (computerSelection==="scissors") {
-        ps++;
-        return "rock wins!" + " " + "You beat the computer,nice Job!";
-        }else {
-        cs++;
-        return "paper wins!" + " " + "Computer beats you,!";
-        }
-      }
-
-      else if (playerSelection==="paper") {
-        if (computerSelection==="scissors") {
-        cs++;
-        return "scissors wins!" + " " + "Computer beats you,!";
-        }else {
-        ps++;
-        return "paper wins!" + " " + "You beat the computer,nice Job!";
-        }
-      }
-
-      else if (playerSelection==="scissors") {
-        if (computerSelection==="paper") {
-        ps++;
-        return "scissors wins!" + " " + "You beat the computer,nice Job!";
-        }else {
-        cs++;
-        return "rock wins!" + " " + "Computer beats you,!";
-        }
-       }
-       else {
-       return "Check what you type !!!"
-       }
+  if (playerSelection === computerSelection) {
+    results.innerHTML = " Tie!" + "Let's play Again.";
+  } else if (playerSelection === "rock") {
+    if (computerSelection === "scissors") {
+      ps++;
+      results.innerHTML = "rock wins!" + " " + "You beat the computer,nice Job!";
+    } else {
+      cs++;
+      results.innerHTML = "paper wins!" + " " + "Computer beats you,!";
     }
-
-    function game(){
-
-      while (ps<3 && cs<3) {
-        alert(playRound(playerSelection,ComputerPlay()));
-        if(ps==3){
-          alert("YOU WIN. YOU : " + ps + " and COMPUTER : " + cs );
-          alert("Refresh The Page To Play Again");
-          break;
-        }else if (cs==3) {
-          alert("COMPUTER WINS. COMPUTER : " + cs + " and YOU : " + ps);
-          alert("Refresh The Page To Play Again");
-          break;
-        }
-      }
+  } else if (playerSelection === "paper") {
+    if (computerSelection === "scissors") {
+      cs++;
+      results.innerHTML = "scissors wins!" + " " + "Computer beats you,!";
+    } else {
+      ps++;
+      results.innerHTML = "paper wins!" + " " + "You beat the computer,nice Job!";
     }
-
-    function myfunction(){
-      game();
+  } else if (playerSelection === "scissors") {
+    if (computerSelection === "paper") {
+      ps++;
+      results.innerHTML = "scissors wins!" + " " + "You beat the computer,nice Job!";
+    } else {
+      cs++;
+      results.innerHTML = "rock wins!" + " " + "Computer beats you,!";
     }
+  }
 
+  scoreBoard();
 
-    // const playerSelection ="rock";
-    // const computerSelection = ComputerPlay()
-    // console.log(playRound(playerSelection,computerSelection))
+}
+
+function scoreBoard() {
+
+  scores.innerHTML = "YourScore : " + ps + " <br> " + "ComputerScore :" + cs;
+  if (ps == 5) {
+    winner.innerHTML = "YOU WIN";
+    resetGame();
+  } else if (cs == 5) {
+    winner.innerHTML = "COMPUTER WINS";
+    resetGame();
+  }
+}
+
+function resetGame() {
+  var resetButton = document.createElement("BUTTON");
+  document.body.appendChild(resetButton);
+  resetButton.innerHTML = "PLAY AGAIN";
+  resetButton.style.color = "white";
+  resetButton.style.backgroundColor = "#d24f4f";
+  resetButton.style.height = "70px";
+  resetButton.style.weight = "100px";
+  resetButton.style.borderRadius = "20px";
+  resetButton.style.borderColor = "orange";
+
+  resetButton.addEventListener("click", function() {
+    ps = 0,
+    cs = 0,
+    resetButton.style.display = "none",
+    winner.innerHTML = "";
+    scores.innerHTML = "";
+    results.innerHTML = "";
+
+  });
+}
